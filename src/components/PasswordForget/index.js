22f8f24e1a174dae297672eeac8821a25742d671
39981838 <shamona9594@gmail.com>
2020-01-05 11:22:23 +0330

    adding authorization logic with hoc and using conditions

--- src/components/PasswordForget/index.js
+++ src/components/PasswordForget/index.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { withFirebase } from "../Firebase";
 import * as ROUTES from "../../constants/routes";
+
 const PasswordForgetPage = () => (
   <div>
     <h1>PasswordForget</h1>
