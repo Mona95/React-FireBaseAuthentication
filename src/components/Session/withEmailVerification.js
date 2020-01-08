70cfd89cb8567efd01c486956ff042a2f4bcd6b2
39981838 <shamona9594@gmail.com>
2020-01-08 21:09:10 +0330

    continue with email verification and using hoc

---
+++ src/components/Session/withEmailVerification.js
@@ -0,0 +1,41 @@
+import React from "react";
+import AuthUserContext from "./context";
+import { withFirebase } from "../Firebase";
+
+const needsEmailVerification = authUser =>
+  authUser &&
+  !authUser.emailVerified &&
+  authUser.providerData
+    .map(provider => provider.providerId)
+    .includes("password");
+
+const withEmailVerification = Component => {
+  class WithEmailVerification extends React.Component {
+    onSendEmailVerification = () => {
+      this.props.firebase.doSendEmailVerification();
+    };
+    render() {
+      return (
+        <AuthUserContext.Consumer>
+          {authUser =>
+            needsEmailVerification(authUser) ? (
+              <div>
+                <p>
+                  Verify your E-Mail: Check you E-Mails (Spam folder included)
+                  for a confirmation E-Mail or send another confirmation E-Mail.
+                </p>
+                <button type="button" onClick={this.onSendEmailVerification}>
+                  Send confirmation E-Mail
+                </button>
+              </div>
+            ) : (
+              <Component {...this.props} />
+            )
+          }
+        </AuthUserContext.Consumer>
+      );
+    }
+  }
+  return withFirebase(WithEmailVerification);
+};
+export default withEmailVerification;
