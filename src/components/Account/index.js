d30cfea3a1ea3efed8f4b98276e35b284eb11bbd
39981838 <shamona9594@gmail.com>
2020-01-08 20:42:23 +0330

    login Managment and active/deactive added, also linking to social logins implemented

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -2,6 +2,22 @@ import React from "react";
 import { PasswordForgetForm } from "../PasswordForget";
 import PasswordChangeForm from "../PasswordChange";
 import { AuthUserContext, withAuthorization } from "../Session";
+import { withFirebase } from "../Firebase";
+
+const SIGN_IN_METHODS = [
+  {
+    id: "password",
+    provider: null
+  },
+  {
+    id: "google.com",
+    provider: "googleProvider"
+  },
+  {
+    id: "twitter.com",
+    provider: "twitterProvider"
+  }
+];
 
 const AccountPage = () => (
   <AuthUserContext.Consumer>
@@ -10,10 +26,88 @@ const AccountPage = () => (
         <h1>Account: {authUser.email}</h1>
         <PasswordForgetForm />
         <PasswordChangeForm />
+        <LoginManagement authUser={authUser} />
       </div>
     )}
   </AuthUserContext.Consumer>
 );
+
+class LoginManagementBase extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeSignInMethods: [],
+      error: null
+    };
+  }
+  componentDidMount() {
+    this.fetchSignInMethods();
+  }
+
+  fetchSignInMethods = () => {
+    this.props.firebase.auth
+      .fetchSignInMethodsForEmail(this.props.authUser.email)
+      .then(activeSignInMethods =>
+        this.setState({ activeSignInMethods, error: null })
+      )
+      .catch(error => this.setState({ error }));
+  };
+
+  onSocialLoginLink = provider => {
+    this.props.firebase.auth.currentUser
+      .linkWithPopup(this.props.firebase[provider])
+      .then(this.fetchSignInMethods)
+      .catch(error => this.setState({ error }));
+  };
+
+  onUnLink = providerId => {
+    this.props.firebase.auth.currentUser
+      .unlink(providerId)
+      .then(this.fetchSignInMethods)
+      .catch(error => this.setState({ error }));
+  };
+
+  render() {
+    const { activeSignInMethods, error } = this.state;
+    return (
+      <div>
+        Sign In Methods:
+        <ul>
+          {SIGN_IN_METHODS.map(signInMethod => {
+            const onlyOneLeft = activeSignInMethods.length === 1;
+            const isEnabled = activeSignInMethods.includes(signInMethod.id);
+            return (
+              <li key={signInMethod.id}>
+                {isEnabled ? (
+                  <button
+                    type="button"
+                    onClick={() => this.onUnlink(signInMethod.id)}
+                    disabled={onlyOneLeft}
+                  >
+                    Deactivate {signInMethod.id}
+                  </button>
+                ) : (
+                  <button
+                    type="button"
+                    onClick={() =>
+                      this.onSocialLoginLink(signInMethod.provider)
+                    }
+                  >
+                    Link {signInMethod.id}
+                  </button>
+                )}
+              </li>
+            );
+          })}
+        </ul>
+        {error && error.message}
+      </div>
+    );
+  }
+}
+
+const LoginManagement = withFirebase(LoginManagementBase);
+
 const condition = authUser => !!authUser;
 
 export default withAuthorization(condition)(AccountPage);
