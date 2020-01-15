d6f8167539ff0b246e678cebe7a40e4b666ccedc
39981838 <shamona9594@gmail.com>
2020-01-15 21:33:11 +0330

    worked on MessageBase component in Home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
-import { withAuthorization, withEmailVerification } from "../Session";
+import {
+  AuthUserContext,
+  withAuthorization,
+  withEmailVerification
+} from "../Session";
 import { withFirebase } from "../Firebase";
 
 const Home = () => (
@@ -11,10 +15,81 @@ const Home = () => (
 );
 
 class MessagesBase extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      messages: [],
+      text: ""
+    };
+  }
+
+  onChangeText = event => {
+    this.setState({ text: event.target.value });
+  };
+
+  onCreateMessage = (event, authUser) => {
+    this.props.firebase.messages().push({
+      text: this.state.text,
+      userId: authUser.uid
+    });
+
+    this.setState({ text: "" });
+
+    event.preventDefault();
+  };
+
+  componentDidMount() {
+    this.setState({ loading: true });
+    this.props.firebase.messages().on("value", snapshot => {
+      const messageObject = snapshot.val();
+      if (messageObject) {
+        // convert messages list from snapshot
+        this.setState({ loading: false });
+      } else {
+        this.setState({ messages: null, loading: false });
+      }
+    });
+  }
+  componentWillUnmount() {
+    this.props.firebase.messages().off();
+  }
   render() {
-    return <div>Messages will be here</div>;
+    const { text, messages, loading } = this.state;
+    return (
+      <AuthUserContext.Consumer>
+        {authUser => (
+          <div>
+            {loading && <div>Loading ...</div>}
+            {messages ? (
+              <MessageList messages={messages} />
+            ) : (
+              <div>There are no messages ...</div>
+            )}
+            <form onSubmit={event => this.onCreateMessage(event, authUser)}>
+              <input type="text" value={text} onChange={this.onChangeText} />
+              <button type="submit">Send</button>
+            </form>
+          </div>
+        )}
+      </AuthUserContext.Consumer>
+    );
   }
 }
+const MessageList = ({ messages }) => {
+  return (
+    <ul>
+      {messages.map(message => (
+        <MessageItem key={message.uid} message={message} />
+      ))}
+    </ul>
+  );
+};
+const MessageItem = ({ message }) => (
+  <li>
+    <strong>{message.userId}</strong> {message.text}
+  </li>
+);
 const Messages = withFirebase(MessagesBase);
 
 const condition = authUser => !!authUser;
